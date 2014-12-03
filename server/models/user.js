ab98e58cbfb02e85998a1e15995215e94cd219cb
Chyld Medford <chyld.medford@gmail.com>
2014-12-03 13:40:56 -0600

    register

--- server/models/user.js
+++ server/models/user.js
@@ -7,33 +7,40 @@ var bcrypt  = require('bcrypt'),
     crypto  = require('crypto'),
     pg      = require('../postgres/manager');
 
-function User(){
+function User(obj){
+  this.username = obj.username;
 }
 
 User.register = function(obj, cb){
+  var user = new User(obj);
+  user.password = bcrypt.hashSync(obj.password, 8);
 
-  download(obj.avatar, function(url){
-    console.log('the url on s3 is', url);
-    cb();
+  randomUrl(obj.avatar, function(file, avatar){
+    user.avatar = avatar;
+    pg.query('insert into users (username, password, avatar) values ($1, $2, $3) returning id', [user.username, user.password, user.avatar], function(err, results){
+      if(err){return cb(true);}
+      download(obj.avatar, file, cb);
+    });
   });
-
 };
 
-function download(url, cb){
-  var s3 = new AWS.S3(),
-  ext  = path.extname(url);
+function randomUrl(url, cb){
+  var ext  = path.extname(url);
 
   crypto.randomBytes(48, function(ex, buf){
     var token  = buf.toString('hex'),
         file   = token + '.avatar' + ext,
         avatar = 'https://s3.amazonaws.com/' + process.env.AWS_BUCKET + '/' + file;
+    cb(file, avatar);
+  });
+}
 
-    request({url: url, encoding: null}, function(err, response, body){
-      var params = {Bucket: process.env.AWS_BUCKET, Key: file, Body: body, ACL: 'public-read'};
-      s3.putObject(params, function(){
-        cb(avatar);
-      });
-    });
+function download(url, file, cb){
+  var s3 = new AWS.S3();
+
+  request({url: url, encoding: null}, function(err, response, body){
+    var params = {Bucket: process.env.AWS_BUCKET, Key: file, Body: body, ACL: 'public-read'};
+    s3.putObject(params, cb);
   });
 }
 
