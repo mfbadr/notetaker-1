d49fb04a1b6041bca4afb4bc9c8bce841250aa56
Chyld Medford <chyld.medford@gmail.com>
2014-12-04 14:01:46 -0600

    add_note sql function

--- server/models/note.js
+++ server/models/note.js
@@ -6,7 +6,7 @@ function Note(){
 }
 
 Note.create = function(user, obj, cb){
-  pg.query('select add_note($1, $2, $3, $4);', [user.id, obj.title, obj.body, obj.tags], function(err, results){
+  pg.query('select add_note($1, $2, $3, $4)', [user.id, obj.title, obj.body, obj.tags], function(err, results){
     console.log(err, results);
     cb();
   });
