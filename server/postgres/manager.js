5afaf10e81277c314f9825578f133fe398e35938
Mikey Badr <mfbadr@gmail.com>
2014-12-05 11:49:01 -0600

    change to database url

--- server/postgres/manager.js
+++ server/postgres/manager.js
@@ -3,7 +3,7 @@ var pg = require('pg');
 
 exports.query = function(sql, params, cb){
 
-  pg.connect(process.env.DB, function(err, client, done){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if(err){return cb(err);}
 
     /* CONNECTION POOL DEBUGGING */
