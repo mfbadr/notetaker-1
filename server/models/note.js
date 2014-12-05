6aa86d02b068ab9620934fe2bc417f9bd81cf1ac
Mikey Badr <mfbadr@gmail.com>
2014-12-05 09:52:51 -0600

    improve query

--- server/models/note.js
+++ server/models/note.js
@@ -13,7 +13,10 @@ Note.create = function(user, obj, cb){
 };
 
 Note.list = function(userID, cb){
-  pg.query('select notes.title, notes.created_at from notes inner join users on notes.user_id=users.id where notes.user_id=' + userID + ';', [], function(err, results){
+  pg.query('select notes.title, notes.created_at, notes.id ' +
+  'from notes ' +
+  'inner join users on notes.user_id=users.id ' +
+  'where notes.user_id= $1;', [userID], function(err, results){
     console.log(err, results);
     cb(err, results.rows);
   });
