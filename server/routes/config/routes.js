be5dc3040b142cf64558381c273dc7ba4d658ad6
Mikey Badr <mfbadr@gmail.com>
2014-12-10 14:53:48 -0600

    mobile upload works

--- server/routes/config/routes.js
+++ server/routes/config/routes.js
@@ -10,5 +10,6 @@ module.exports = [
   {method: 'get',    path: '/notes',                 config: require('../definitions/notes/list')},
   {method: 'post',   path: '/notes/{noteId}/upload', config: require('../definitions/notes/upload')},
   {method: 'get',    path: '/notes/{noteId}',        config: require('../definitions/notes/one')},
+  {method: 'post',   path: '/mobileupload',         config: require('../definitions/notes/mobileupload')},
   {method: 'get',    path: '/notes/count',           config: require('../definitions/notes/count')}
 ];
---
+++ server/routes/definitions/notes/mobileupload.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Joi  = require('joi'),
+    Note = require('../../../models/note');
+
+module.exports = {
+  description: 'Upload a Photo from mobile',
+  tags:['notes'],
+  validate: {
+    payload: {
+      noteId: Joi.number().required(),
+      b64: Joi.string().required()
+    }
+  },
+  handler: function(request, reply){
+    console.log('BEFORE UPLOAD PAYLOAD', request.payload);
+    Note.mobileUpload(request.payload.noteId, request.payload.b64, request.auth.credentials, function(err){
+      if(err){
+        console.log('MOBILE UPLOAD ERROR', err);
+      }
+      reply().code(err ? 400 : 200);
+    });
+  }
+};
